f63f12cb5f480490a8637633f4bb596801395f6f
SkaMateria <skamateria@gmail.com>
2019-10-31 15:21:49 -0400

    multiple fetch working

--- frontend-rigassist/src/App.js
+++ frontend-rigassist/src/App.js
@@ -9,24 +9,36 @@ const USERS_URL = `${BASE_URL}/users`
 const PROJECTS_URL = `${BASE_URL}/projects`
 const ANIMATIONS_URL = `${BASE_URL}/animations`
 
-let something = "nothing"
 class App extends React.Component {
   constructor(){
     super()
     this.state = {
       allUsers: [],
       allAnimations: [],
+      allProjects: []
     }
   }
 
-
   componentDidMount() {
-    fetch(USERS_URL)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
+    Promise.all([
+      fetch(USERS_URL),
+      fetch(PROJECTS_URL),
+      fetch(ANIMATIONS_URL)
+    ])
+    .then(([res1, res2, res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
+    .then(([data1, data2, data3]) => {
+      console.log("users:",data1)
+      console.log("projects:",data2)
+      console.log("animations:",data3)
     })
   }
+  // componentDidMount() {
+  //   fetch(USERS_URL)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+  // }
 
 
   render(){
