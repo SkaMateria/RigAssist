1dc5c4e87f964282f868022d08343b4adc397a99
SkaMateria <skamateria@gmail.com>
2019-10-31 14:49:44 -0400

    Corrected router error

--- frontend-rigassist/src/App.js
+++ frontend-rigassist/src/App.js
@@ -4,15 +4,29 @@ import NavBar from './containers/NavBar'
 import WorkBench from './containers/WorkBench';
 import FolderNavigator from './containers/FolderNavigator'
 
+const URL = ""
 
-function App() {
-  return (
-    <div>
-      <NavBar/>
-      <FolderNavigator/>
-      <WorkBench/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      allUsers: [],
+      allAnimations: [],
+    }
+  }
+
+
+
+
+  render(){
+    return (
+      <div>
+        <NavBar/>
+        <FolderNavigator/>
+        <WorkBench/>
+      </div>
+    );
+  }
 }
 
 export default App;
