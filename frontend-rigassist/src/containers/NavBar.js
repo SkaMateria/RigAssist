a0b2cd1bbc68d78fe8cba9917969d58d4a3d9e5c
SkaMateria <skamateria@gmail.com>
2019-10-30 23:01:11 -0400

    Double checking

--- frontend-rigassist/src/containers/NavBar.js
+++ frontend-rigassist/src/containers/NavBar.js
@@ -4,6 +4,7 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
+            Just a double check
                 NavBarContainer
             </div>
         )
