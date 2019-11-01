557c8ae7644e4b07ea9b96c1ccafdbd6ea41cd52
SkaMateria <skamateria@gmail.com>
2019-11-01 00:02:45 -0400

    being able to retrieve css style information and filter

--- frontend-rigassist/src/containers/WorkBench.js
+++ frontend-rigassist/src/containers/WorkBench.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
-import ControlPanel from './ControlPanel'
+import React, { Component } from 'react';
+import ControlPanel from './ControlPanel';
+import '../styles/Animations.css'
+import styles from '../styles/Components.css'
 
-export class WorkBench extends Component {
+class WorkBench extends Component {
+	constructor() {
+		super()
+		this.state = {
+			style: "box"
+		}	
+	}
+
+
+	colorChange = () => {
+		// this.setState({
+			// style: "box_clicked"
+		// })
+		let box = document.querySelector('.box')
+		let style = getComputedStyle(box)
+		let styleProps = (({width, height}) => ({width, height}))(style)
+		console.log(styleProps)
+		
+	}	
+
 	render() {
 		return (
 			<div>
+				<div className={this.state.style} onClick={this.colorChange} ></div>
 				<ControlPanel/>
 			</div>
 		)
