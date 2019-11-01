ab5576953c21e771cda8a3af23aee4cf63f68728
SkaMateria <skamateria@gmail.com>
2019-11-01 01:18:33 -0400

    Slowly, but surely I'm getting the hang of styled components

--- frontend-rigassist/src/containers/WorkBench.js
+++ frontend-rigassist/src/containers/WorkBench.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom'
 import ControlPanel from './ControlPanel';
 import '../styles/Animations.css'
 import styles from '../styles/Components.css'
-import styled, {keyframes} from 'styled-components'
+import styled from 'styled-components'
 
-class WorkBench extends Component {
+const Ball = styled.div`
+	width: 64px;
+	height: 64px;
+	border-radius: 50%;
+	margin: 0 auto;
+	background-color: red;
+`;
+
+export default class WorkBench extends Component {
 	constructor() {
 		super()
 		this.state = {
@@ -14,38 +22,34 @@ class WorkBench extends Component {
 	}
 	
 	
-	const Ball = styled.div`
-		width: 64px;
-		height: 64px;
-		border-radius: 50%;
-		margin: 0 auto;
-		backgroundColor: red;
-	`
 	colorChange = () => {
 		// let box = document.querySelector('.box')
 		// let style = getComputedStyle(box)
 		// let styleProps = (({width, height, backgroundColor}) => ({width, height, backgroundColor}))(style)
 		// console.log("before click:",styleProps)
 		// this.setState({
-		// 	style: "box_clicked"
-		// })
-		// console.log("after click:", styleProps)			
-		// let x = this	
-		// let node = ReactDOM.findDOMNode(this.firstElementChild)
-		// debugger
-		// let x = getComputedStyle(node)
-		// console.log(node)
-	}	
-
-	
-	render() {
-		return (
-			<div>
-				<div className={this.state.style} onClick={this.colorChange} ></div>
-				<Ball/>
+			// 	style: "box_clicked"
+			// })
+			// console.log("after click:", styleProps)			
+			// let x = this	
+			// let node = ReactDOM.findDOMNode(this.firstElementChild)
+			// debugger
+			// let x = getComputedStyle(node)
+			// console.log(node)
+		}	
+		
+		
+		
+		render() {
+			return (
+				<div>
+				{/* <div className={this.state.style} onClick={this.colorChange} ></div> */}
+				<Ball></Ball>
 				<ControlPanel/>
 			</div>
 		)
 	}
 }
-export default WorkBench
+// export default WorkBench
+		
+		
