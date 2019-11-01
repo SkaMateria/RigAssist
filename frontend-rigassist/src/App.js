d189df14298fe02b153e4e3a1ee71d8a79d776ee
SkaMateria <skamateria@gmail.com>
2019-10-31 21:28:05 -0400

    removed test snippets

--- frontend-rigassist/src/App.js
+++ frontend-rigassist/src/App.js
@@ -45,17 +45,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
-
