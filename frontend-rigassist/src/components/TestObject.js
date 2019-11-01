09bf9e97c5712f5020ee0d8cc18b965279578b98
SkaMateria <skamateria@gmail.com>
2019-11-01 17:47:50 -0400

    finalized the method of passing state and props to animation attributes

--- frontend-rigassist/src/components/TestObject.js
+++ frontend-rigassist/src/components/TestObject.js
@@ -1,29 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 
-const Ball = styled.div(props => ({
-	width: "64px",
-	height: "64px",
-	borderRadius: "50%",
-	margin: "0 auto",
-	backgroundColor: "red"
-}));
+const rotate = keyframes`
+	from {
+		transform: rotate(0deg);
+	}
 
+	to {
+		transform: rotate(360deg);
+	}
+`
 
-const Box = styled.div(props => ({
-	width: '64px',
-	height: '64px',
-	margin: '0 auto',
-	backgroundColor: props.color
-}))
-
-const Box2 = styled.div.attrs(props => ({
-	color2: props.color
+const Box = styled.div.attrs(props => ({
+	color: props.color
 }))`
 	width: 64px
 	height: 64px
 	margin: 0 auto
 	background-color: ${props => props.color}
+	animation: ${rotate} 2s linear infinite
 `
 
 export default class TestObject extends React.Component{
@@ -31,7 +26,10 @@ export default class TestObject extends React.Component{
 		super(props)
 		
 		this.state = {
-			backgroundColor: this.props.backgroundColor,
+			color: this.props.backgroundColor,
+			width: "64px",
+			height: "64px",
+			animationLength: "2s linear infinite",
 		}
 	}
 	
@@ -39,16 +37,10 @@ export default class TestObject extends React.Component{
 	clickHandler = () => {
 	}
 
-	getColor = () => {
-		return this.state.backgroundColor
-	}
-
   render() {
 		return(
 			<div>
-        <Box onClick={this.clickHandler} color={this.getColor()} ></Box>
-        <Ball onClick={this.clickHandler} color={this.getColor()} ></Ball>
-        <Box2 onClick={this.clickHandler} color={this.getColor()} ></Box2>
+        <Box onClick={this.clickHandler} color={this.state.color} ></Box>
 			</div>
     )
   }
