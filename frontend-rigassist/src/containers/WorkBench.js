12406444641dcbd1939e4057e471f11fa0a134eb
SkaMateria <skamateria@gmail.com>
2019-11-01 00:48:41 -0400

    Trouble shooting styled components

--- frontend-rigassist/src/containers/WorkBench.js
+++ frontend-rigassist/src/containers/WorkBench.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import ControlPanel from './ControlPanel';
 import '../styles/Animations.css'
 import styles from '../styles/Components.css'
+import styled, {keyframes} from 'styled-components'
 
 class WorkBench extends Component {
 	constructor() {
@@ -10,23 +12,37 @@ class WorkBench extends Component {
 			style: "box"
 		}	
 	}
-
-
+	
+	
+	const Ball = styled.div`
+		width: 64px;
+		height: 64px;
+		border-radius: 50%;
+		margin: 0 auto;
+		backgroundColor: red;
+	`
 	colorChange = () => {
+		// let box = document.querySelector('.box')
+		// let style = getComputedStyle(box)
+		// let styleProps = (({width, height, backgroundColor}) => ({width, height, backgroundColor}))(style)
+		// console.log("before click:",styleProps)
 		// this.setState({
-			// style: "box_clicked"
+		// 	style: "box_clicked"
 		// })
-		let box = document.querySelector('.box')
-		let style = getComputedStyle(box)
-		let styleProps = (({width, height}) => ({width, height}))(style)
-		console.log(styleProps)
-		
+		// console.log("after click:", styleProps)			
+		// let x = this	
+		// let node = ReactDOM.findDOMNode(this.firstElementChild)
+		// debugger
+		// let x = getComputedStyle(node)
+		// console.log(node)
 	}	
 
+	
 	render() {
 		return (
 			<div>
 				<div className={this.state.style} onClick={this.colorChange} ></div>
+				<Ball/>
 				<ControlPanel/>
 			</div>
 		)
