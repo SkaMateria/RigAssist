fc0050a526db32519597b90968bc7e05c9652817
SkaMateria <skamateria@gmail.com>
2019-11-04 11:08:18 -0500

    setting up pause button

--- frontend-rigassist/src/components/TestObject.js
+++ frontend-rigassist/src/components/TestObject.js
@@ -2,12 +2,14 @@ import React from 'react'
 import styled, {keyframes} from 'styled-components'
 
 const rotate = keyframes`
-	from {
-		transform: rotate(0deg);
+	0% {
+		transform:
+			rotate(0deg) scale(0.0)
 	}
 
-	to {
-		transform: rotate(360deg);
+	100% {
+		transform: 
+			rotate(360deg) scale(1.5)
 	}
 `
 
@@ -15,15 +17,26 @@ const Box = styled.div.attrs(props => ({
 	color: props.state.color,
 	animationLength: props.state.animationLength,
 }))`
-	width: 64px
-	height: 64px
+	width: 50px
+	height: 50px
+	position: relative
+	left: 0%
+	bottom: 0px
 	margin: 0 auto
 	background-color: ${props => props.color}
 	animation: ${rotate} ${props => props.animationLength}
+	animation-play-state: ${props => props.animationPlayState}
 `
 		/*//& --NOTE--
 			UrlRef = https://www.w3schools.com/cssref/css3_pr_animation.asp
-			//! animation: name | duration | timing-function | delay | iteration-count | direction | fill-mode | play-state
+			//* if the word has @ in front of it, it should read out "animation-" i.e. @duration = animation-duration
+			//: animation: name | @duration | @timing-function | @delay | @iteration-count | @direction | @fill-mode | @play-state
+			//! cubic-bezier: 0.0-1.0
+			//? duration: in seconds || timing-function: ease/linear/ease-in/ease-out/ease-in-out/cubic-bezier(n,n,n,n)
+			//? dealy: in seconds (can be negative) ||  iteration-count: 0 - infinite  ||  
+			//? direction: normal/reverse/alternate/alternate-reverse
+			//: fill-mode: none/forwards/backwards/both
+			//: play-state: paused/running/initial/inherit
 			*/
 
 
@@ -35,18 +48,20 @@ export default class TestObject extends React.Component{
 			color: this.props.backgroundColor,
 			width: "64px",
 			height: "64px",
-			animationLength: "2s linear 10 reverse",
+			animationLength: "2s ease-in-out infinite normal",
+			animationPlayState: "running"
 		}
 	}
 	
 	
-	clickHandler = () => {
+	pauseClick = () => {
+		
 	}
 
   render() {
 		return(
 			<div>
-        <Box onClick={this.clickHandler} state={this.state} ></Box>
+        <Box onClick={this.pauseClick} state={this.state} ></Box>
 			</div>
     )
   }
