b16d002d27ca9f9eab8b0d1f6aca52d9a1a2cccc
skamateria <skamateria@gmail.com>
2019-12-28 05:19:23 -0500

    animation stage for 25-75 added to sliders

--- frontend-rigassist/src/components/workbench/Slider.js
+++ frontend-rigassist/src/components/workbench/Slider.js
@@ -17,12 +17,30 @@ export class AnimationSlider extends Component {
       value,
     });
   }
+  // onAfterChange = (value) => {
+  //   let x = this.props.setting()
+  //   this.props.animationStage === 0 ? 
+  //     this.props.sliderHandle0(x, value)
+  //     :
+  //     this.props.sliderHandle100(x, value)
+  // }
   onAfterChange = (value) => {
     let x = this.props.setting()
-    this.props.animationStage === 0 ? 
+    if (this.props.animationStage === 0) {
       this.props.sliderHandle0(x, value)
-      :
+    }
+    else if (this.props.animationStage === 25) {
+      this.props.sliderHandle25(x, value)
+    }
+    else if (this.props.animationStage === 50) {
+      this.props.sliderHandle50(x, value)
+    }
+    else if (this.props.animationStage === 75) {
+      this.props.sliderHandle75(x, value)
+    }
+    else if (this.props.animationStage === 100) {
       this.props.sliderHandle100(x, value)
+    }
   }
 
 
